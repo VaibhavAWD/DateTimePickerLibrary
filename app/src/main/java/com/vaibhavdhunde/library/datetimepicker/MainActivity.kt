9c7ca00a7f799a3c13f30e9ec0422bdebe1a833e
Vaibhav Dhunde <vaibhavdhunde94@gmail.com>
2019-09-21 21:16:30 +0530

    Test datepickerutil to select and display date

--- app/src/main/java/com/vaibhavdhunde/library/datetimepicker/MainActivity.kt
+++ app/src/main/java/com/vaibhavdhunde/library/datetimepicker/MainActivity.kt
@@ -2,11 +2,37 @@ package com.vaibhavdhunde.library.datetimepicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vaibhavdhunde.library.datepickerutil.DatePickerDialogFragment
+import com.vaibhavdhunde.library.datepickerutil.getDate
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DatePickerDialogFragment.OnDateSetListener {
+
+    private val selectedDate = Calendar.getInstance().time.time
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showDate(selectedDate) // initial date
+
+        btn_select_date.setOnClickListener {
+            getDate(this)
+        }
+    }
+
+    override fun onDateSet(timeInMillis: Long) {
+        showDate(timeInMillis)
+    }
+
+    private fun showDate(timeInMillis: Long) {
+        text_date.text = getDisplayDate(timeInMillis)
+    }
+
+    private fun getDisplayDate(timeInMillis: Long): String {
+        return SimpleDateFormat("d MMM, yyyy", Locale.getDefault())
+            .format(Date(timeInMillis))
     }
 }
