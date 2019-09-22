6fd17d04ad886193415a1c060e3907ab86bddfe8
Vaibhav Dhunde <vaibhavdhunde94@gmail.com>
2019-09-22 13:03:46 +0530

    Test use timepickerutil to display time

--- app/src/main/java/com/vaibhavdhunde/library/datetimepicker/MainActivity.kt
+++ app/src/main/java/com/vaibhavdhunde/library/datetimepicker/MainActivity.kt
@@ -4,28 +4,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vaibhavdhunde.library.datepickerutil.DatePickerDialogFragment
 import com.vaibhavdhunde.library.datepickerutil.DatePickerUtil
+import com.vaibhavdhunde.library.timepickerutil.TimePickerDialogFragment
+import com.vaibhavdhunde.library.timepickerutil.TimePickerUtil
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-class MainActivity : AppCompatActivity(), DatePickerDialogFragment.OnDateSetListener {
+class MainActivity : AppCompatActivity(), DatePickerDialogFragment.OnDateSetListener,
+    TimePickerDialogFragment.OnTimeSetListener {
 
     private var selectedDate = Calendar.getInstance().time.time
+    private var selectedTime = Calendar.getInstance().time.time
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         showDate(selectedDate) // initial date
+        showTime(selectedTime) // initial time
 
         btn_select_date.setOnClickListener {
             DatePickerUtil.getDate(this, selectedDate)
+        }
+
+        btn_select_time.setOnClickListener {
+            TimePickerUtil.getTime(this, true, selectedTime)
         }
     }
 
     override fun onDateSet(timeInMillis: Long) {
         selectedDate = timeInMillis
         showDate(timeInMillis)
+    }
+
+    override fun onTimeSet(timeInMillis: Long) {
+        selectedTime = timeInMillis
+        showTime(timeInMillis)
     }
 
     private fun showDate(timeInMillis: Long) {
@@ -35,5 +49,14 @@ class MainActivity : AppCompatActivity(), DatePickerDialogFragment.OnDateSetList
     private fun getDisplayDate(timeInMillis: Long): String {
         return SimpleDateFormat("d MMM, yyyy", Locale.getDefault())
             .format(Date(timeInMillis))
+    }
+
+    private fun showTime(timeInMillis: Long) {
+        text_time.text = getDisplayTime(timeInMillis)
+    }
+
+    private fun getDisplayTime(timeInMillis: Long): String {
+        return SimpleDateFormat("h:mm a", Locale.getDefault())
+            .format(Date(timeInMillis)).toUpperCase(Locale.getDefault())
     }
 }
